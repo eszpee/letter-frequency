cea1eca1a4e21dab3bbaabead366053d4d08e2dd
Péter Szász <eszpee@gmail.com>
2022-01-07 09:05:48 +0100

    Words found

--- letters.js
+++ letters.js
@@ -1,10 +1,8 @@
 "use strict";
 const fs = require("fs");
-
 const wordlist = './wordlist.txt';
 const wordArray = readInputArray(wordlist);
 
-// for frequency, iterate through lines of file
 let freq = [];
 wordArray.forEach(function(word) {
     if (word.length == 5) {
@@ -20,13 +18,29 @@ wordArray.forEach(function(word) {
     }
 });
 console.log("Letters by frequency in 5-letter words (not counting multiple occurences within same word):");
+let freqSorted = [];
 Object.keys(freq).sort((a,b) => freq[b] - freq[a]).forEach(function(letter) {
     console.log(letter,freq[letter]);
+    freqSorted.push(letter);
 })
 
-
-
-
+const charsToFind = freqSorted.slice(0,5);
+console.log("Finding words with most common letters",charsToFind.join(', '));
+let matches = [];
+wordArray.forEach(function(word) {
+    if (word.length == 5) {
+        let found = true;
+        charsToFind.forEach(function(chr) {
+            if (!word.includes(chr)) {
+                found = false;
+            }
+        });
+        if (found) {
+            matches.push(word);
+        }
+    }
+});
+console.log(matches);
 
 
 function readInputArray(path) {
