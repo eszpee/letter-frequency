76e7dccc883eb15d6129616182d9dac19096b1fc
Péter Szász <eszpee@gmail.com>
2022-01-07 08:56:40 +0100

    Frequency done

--- letters.js
+++ letters.js
@@ -1,12 +1,29 @@
 "use strict";
 const fs = require("fs");
 
-const wordlist = './wordlist_test.txt';
-
+const wordlist = './wordlist.txt';
 const wordArray = readInputArray(wordlist);
-console.log(wordArray.length, wordArray[0]);
 
-// get command line arguments
+// for frequency, iterate through lines of file
+let freq = [];
+wordArray.forEach(function(word) {
+    if (word.length == 5) {
+        let letters = [...new Set(word.split(''))];
+        letters.forEach(function(letter) {
+            if(freq[letter] === undefined) {
+                freq[letter] = 1;
+            }
+            else {
+                freq[letter]++;
+            }
+        })
+    }
+});
+console.log("Letters by frequency in 5-letter words:");
+Object.keys(freq).sort((a,b) => freq[b] - freq[a]).forEach(function(letter) {
+    console.log(letter,freq[letter]);
+})
+
 function readInputTxt(path) {
     return fs.readFileSync(path, 'utf8');
 }
@@ -14,5 +31,4 @@ function readInputArray(path) {
     return readInputTxt(path).split(/\r?\n/);
 }
 
-// for frequency, iterate through lines of file
 
